057d062a247ed579004b51fbf6f4d93436c894fc
IsaqueSantos-Sale <isaque.santos@after.sale>
2024-01-07 12:34:47 -0300

    feature: create jump and falling down

--- app/Objects/Mage/Mage.ts
+++ app/Objects/Mage/Mage.ts
@@ -5,14 +5,53 @@ import solidColliderBox2 from "@Src/Physical/solidColliderBox2";
 
 export default class Mage {
   sprite = new Box2(100, 100, 30, 60);
+  inFloor = false;
+  falling = {
+    acceleration: 1,
+    velocity: 0,
+  };
+  jump = {
+    heigth: 0,
+    maxHeigth: 50,
+    acceleration: 1,
+    velocity: 0,
+    maxJumps: 2,
+    totalJumps: 0,
+    actived: false,
+    inAir: false,
+  };
 
   constructor() {
     this.sprite.fillColor = "white";
   }
 
-  onJump() {}
+  onJump() {
+    const { isDown, reset } = keyboard;
 
-  onFallingDown() {}
+    if (this.inFloor && isDown("Space")) {
+      this.inFloor = false;
+      this.jump.actived = true;
+      reset("Space");
+    }
+
+    if (this.jump.actived && this.jump.heigth <= this.jump.maxHeigth) {
+      this.jump.velocity += this.jump.acceleration;
+      this.jump.heigth += this.jump.velocity;
+      this.sprite.position.y -= this.jump.velocity;
+      return;
+    }
+
+    this.jump.actived = false;
+    this.jump.velocity = 0;
+    this.jump.heigth = 0;
+  }
+
+  onFallingDown() {
+    if (this.jump.actived) return;
+
+    this.falling.velocity += this.falling.acceleration;
+    this.sprite.position.y += this.falling.velocity;
+  }
 
   onMovimentationX() {
     const { position } = this.sprite;
@@ -52,6 +91,8 @@ export default class Mage {
     for (const floor of floors) {
       solidColliderBox2(this.sprite, floor.sprite, {
         onTop: ({ overlapY }) => {
+          this.inFloor = true;
+          this.falling.velocity = 0;
           this.sprite.position.y -= overlapY;
         },
         onLeft: ({ overlapX }) => {
