def97d6cd498fd7a70d45ae77eaf1dc0c5fc8e0f
IsaqueSantos-Sale <isaque.santos@after.sale>
2024-01-08 10:49:41 -0300

    refectory staff

--- app/Objects/Staff/Staff.ts
+++ app/Objects/Staff/Staff.ts
@@ -1,49 +1,33 @@
 import Box2 from "@Src/Draw/Box2";
 import Mage from "../Mage";
 import { canvas, mouse } from "@Src/index";
-import Magic from "../Magic";
 import relativeWith from "@Src/Maths/relativeWith";
 import rotateToDirection from "@Src/Maths/rotateToDirection";
 
 export default class Staff {
   sprite = new Box2(0, 0, 70, 4);
-  directionX: number = 0;
-  directionY: number = 0;
-
-  magic = new Magic();
 
   constructor(private readonly mage: Mage) {
     this.sprite.fillColor = "brown";
-    this.sprite.origin.x -= 10;
   }
 
-  onThrowMagic() {
+  onRotate() {
     rotateToDirection(this.sprite, this.sprite.position, mouse);
-    relativeWith(this.magic.sprite, this.sprite, {
-      x: this.sprite.size.x - this.magic.sprite.radius * 2,
-      y: this.sprite.size.halfY(),
-    });
   }
 
   onFixInMage() {
-    this.sprite.position.set(
-      this.mage.sprite.position.x +
-        this.mage.sprite.size.x / 2 -
-        this.sprite.origin.x,
-      this.mage.sprite.position.y +
-        this.mage.sprite.size.y / 2 -
-        this.sprite.origin.y
-    );
+    relativeWith(this.sprite, this.mage.sprite, {
+      x: this.mage.sprite.size.halfX() - this.sprite.size.halfX() + 10,
+      y: this.mage.sprite.size.halfY(),
+    });
   }
 
   update() {
     this.onFixInMage();
-    this.onThrowMagic();
-    this.magic.update();
+    this.onRotate();
   }
 
   render() {
-    this.magic.render();
     this.sprite.fill(canvas.context);
   }
 }
