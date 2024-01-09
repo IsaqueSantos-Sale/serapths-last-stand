5894315abfe191999ee51adb2070d7c95bf2ce32
IsaqueSantos-Sale <isaque.santos@after.sale>
2024-01-09 20:27:50 -0300

    refactory: refactory time

--- src/Events/Time.ts
+++ src/Events/Time.ts
@@ -1,16 +1,22 @@
 export default class Time {
   private _prevTime: number = 0;
   private _deltaTime: number = 0;
+  private _timeStamp: number = 0;
 
-  constructor() {
-    this.delta = this.delta.bind(this);
+  fps() {
+    return 1000 / this.delta();
   }
 
   delta() {
     return this._deltaTime;
   }
 
+  timeStamp() {
+    return this._timeStamp;
+  }
+
   handle(timeStamp: number) {
+    this._timeStamp = timeStamp;
     this._deltaTime = timeStamp - this._prevTime;
     this._prevTime = timeStamp;
   }
